88d5ec88b5659c0a3920077377ea27670b800cfe
Carlos Canet <carlos@carlosca.net>
2025-07-28 17:00:11 +0200

    question constructor done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,12 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;                // We are "linking" the reference of the array
+        // this.choices = choices.slice(0);    // We can clone the argument instead
+        this.answer = answer;
+        this.difficulty = difficulty;       // Note: The difficulty will be a number between 1 and 3, with 1 being the easiest and 3 being the hardest.
+    }
 
+    // YOUR CODE HERE:
     // 2. shuffleChoices()
 }
