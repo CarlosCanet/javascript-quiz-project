7d8f26c4cd2210e66d368c81a94caa64d38d4e4f
Maël Odina Eugène <maodinae@gmail.com>
2025-07-28 17:14:21 +0200

    yate

--- src/question.js
+++ src/question.js
@@ -7,6 +7,10 @@ class Question {
         this.difficulty = difficulty;       // Note: The difficulty will be a number between 1 and 3, with 1 being the easiest and 3 being the hardest.
     }
 
-    // YOUR CODE HERE:
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) { 
+            const j = Math.floor(Math.random() * (i + 1)); 
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]; 
+        }
+    }
 }
