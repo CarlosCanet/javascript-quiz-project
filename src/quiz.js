b505de3b2f1b965497ec95649ccff4000a270f2a
Alejandro Duque <alegaduq@gmail.com>
2025-07-28 16:57:22 +0100

    getQuestion() method Done

--- src/quiz.js
+++ src/quiz.js
@@ -1,31 +1,24 @@
 class Quiz {
-    
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+   
+    return this.questions[this.currentQuestionIndex]
+  }
 
-    // 4. shuffleQuestions()
+    //should return the item from the questions array at the position of currentQuestionIndex.
 
-    // 5. checkAnswer(answer)
+  // 3. moveToNextQuestion()
 
-    // 6. hasEnded()
+  // 4. shuffleQuestions()
+
+  // 5. checkAnswer(answer)
+
+  // 6. hasEnded()
 }
-
-/*class Quiz
-
-should receive 3 arguments in the constructor (questions, timeLimit, timeRemaining)
-should have 5 properties: questions, timeLimit, timeRemaining, correctAnswers, currentQuestionIndex
-should receive questions (array) as its 1st argument and assign it to questions property. The array is meant to contain Question objects
-should receive timeLimit (number) as its 2nd argument and assign it to timeLimit property.
-should receive timeRemaining (number) as its 3rd argument and assign it to timeRemaining property.
-should have a correctAnswers property initially set to 0.
-should have a currentQuestionIndex property initially set to 0.*/
-
